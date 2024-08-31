a7269b79df5ba8d928564c151b8a1eeba0e59712
강인화 <rkddlsghk1998@gmail.com>
2024-08-31 18:40:36 +0900

    refactor: 2단계 - 컨트롤러 분리

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,25 +1,20 @@
 package roomescape;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/reservations")
 public class ReservationController {
 
     private final List<Reservation> reservations = new ArrayList<>();
 
-    @GetMapping("/reservation")
-    public String getReservationPage() {
-        return "reservation";
-    }
-
-    @ResponseBody
-    @GetMapping("/reservations")
+    @GetMapping
     public ResponseEntity<List<Reservation>> getReservations() {
         return ResponseEntity.ok(reservations);
     }
---
+++ src/main/java/roomescape/ViewController.java
@@ -0,0 +1,13 @@
+package roomescape;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/reservation")
+    public String getReservationPage() {
+        return "reservation";
+    }
+}
